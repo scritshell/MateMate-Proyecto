387efc111e70c2dd8e0494d28788619a35f57d05
scritshell <sebastiancsibi2@gmail.com>
2025-12-11 20:39:06 +0100

    Añadido opcion de notas por secciones.

--- app/src/main/java/com/example/proyectoajedrez/model/Nota.kt
+++ app/src/main/java/com/example/proyectoajedrez/model/Nota.kt
@@ -5,5 +5,6 @@ data class Nota(
     val userId: String = "",      // ID del usuario (para privacidad)
     val titulo: String = "",
     val contenido: String = "",   // Antes era 'preview', ahora guardamos el contenido real
-    val fecha: Long = 0           // Guardamos la fecha en milisegundos para ordenar mejor
+    val fecha: Long = 0,     // Guardamos la fecha en milisegundos para ordenar mejor
+    val categoria: String = "General" // SPINNER
 )
