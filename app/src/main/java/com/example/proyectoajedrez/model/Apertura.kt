11a5a7796c61c0403b7a6c923a89c1d73f660f64
scritshell <sebastiancsibi2@gmail.com>
2026-01-08 20:26:22 +0100

    Menos comentarios

--- app/src/main/java/com/example/proyectoajedrez/model/Apertura.kt
+++ app/src/main/java/com/example/proyectoajedrez/model/Apertura.kt
@@ -2,7 +2,8 @@ package com.example.proyectoajedrez.model
 
 // Clase de datos que representa una apertura de ajedrez
 data class Apertura(
-    val nombre: String,       // Nombre de la apertura (ej: "Defensa Siciliana")
-    val descripcion: String,  // Descripción o movimientos principales
-    val emoji: String         // Emoji representativo de la apertura
+    val nombre: String,
+    val movimientos: String,
+    val emoji: String,
+    val descripcion: String = ""
 )
